7655a6b14db09c49ddc8f048f161c8a51b24eea9
Justy <jfornalik@gmail.com>
2018-07-10 14:36:46 +0200

    create CardController class

---
+++ js/card/CardController.js
@@ -0,0 +1,8 @@
+export default class CardController {
+    constructor(card, cardView) {
+        this.card = card;
+        this.cardView = cardView;
+        console.log(card);
+        console.log(cardView);
+    }
+}
