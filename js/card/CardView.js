bd2ec32dcc0e31514a876798003f2e0cca3dd223
Justy <jfornalik@gmail.com>
2018-07-10 14:37:16 +0200

    Create CardView class

---
+++ js/card/CardView.js
@@ -0,0 +1,10 @@
+export default class CardView {
+    constructor(cardController) {
+        this.cardController = cardController;
+        this.element = this.createElement();
+    }
+
+    createElement() {
+        return null;
+    }
+}
