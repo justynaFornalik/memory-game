eec2f4376e7c0b37ab1205fb9006d668217cc34f
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-07-11 14:12:29 +0200

    Add flip() method to CardController

--- js/card/CardController.js
+++ js/card/CardController.js
@@ -1,7 +1,15 @@
+import {deck} from "../DeckController.js";
+
 export default class CardController {
     //jak zrobić aby cardController miał dostęp do View?
 
     constructor(card) {
         this.card = card;
+        deck.add(this.card);
+    }
+
+    flip() {
+        this.card.isCovered = false;
+        deck.checkIfPair();
     }
 }
