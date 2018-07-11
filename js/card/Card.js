d82e6972ca3579d4eaf106cf08f3df377ffa33e3
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-07-11 14:29:32 +0200

    Add setter to Card

--- js/card/Card.js
+++ js/card/Card.js
@@ -3,11 +3,15 @@ export default class Card {
     constructor() {
         this.id = this.getRandomId();
         this.isCovered = true;
-        this.image = "brytyjski.jpg";
+        this.image = "";
     }
 
     getRandomId() {
         return '_' + Math.random().toString(36).substr(2, 9);
+    }
+
+    set image(image) {
+        this.image = image;
     }
 }
 
