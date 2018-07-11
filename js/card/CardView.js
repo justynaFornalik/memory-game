bcbfb26b07b1eb99a0d971cad5823c55b59243a5
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-07-11 09:48:07 +0200

    Create createExteriorView method

--- js/card/CardView.js
+++ js/card/CardView.js
@@ -1,10 +1,20 @@
 export default class CardView {
     constructor(cardController) {
         this.cardController = cardController;
-        this.element = this.createElement();
+        this.exteriorView = this.createExteriorView();
     }
 
-    createElement() {
-        return null;
+    createExteriorView() {
+        if ("content" in document.createElement("template")) {
+            var templateElement = $("card"); //HTMLTemplateElement, HTMLTemplateElement.content - DocumentFragment
+            var templateContent = templateElement.content.cloneNode(true);
+            return this.render(templateContent);
+
+        } else {
+            return null;
+            // Find another way to add the rows to the table because
+            // the HTML template element is not supported - TO DO!!!
+        }
     }
+
 }
