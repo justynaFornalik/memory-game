6d55f0cce35533d0f91ef534dabad437bc76e8c3
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-07-11 09:56:29 +0200

    Create getRandomId method

--- js/card/Card.js
+++ js/card/Card.js
@@ -1,2 +1,11 @@
 export default class Card {
+
+    constructor() {
+        this.id = this.getRandomId();
+    }
+
+    getRandomId() {
+        return new Date().getUTCMilliseconds().toString();
+    }
 }
+
