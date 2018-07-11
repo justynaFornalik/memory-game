f9ef5fa9c0278dcbc60f456c944eb55be850ec7a
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-07-11 09:47:53 +0200

    Delete console.log

--- js/Main.js
+++ js/Main.js
@@ -2,7 +2,6 @@ import Card from "./card/Card.js";
 import CardView from "./card/CardView.js";
 import CardController from "./card/CardController.js";
 
-
 for (let i=0; i<10; i++) {
     let card = new Card();
     let cardView = new CardView(card);
--- js/card/CardController.js
+++ js/card/CardController.js
@@ -2,7 +2,5 @@ export default class CardController {
     constructor(card, cardView) {
         this.card = card;
         this.cardView = cardView;
-        console.log(card);
-        console.log(cardView);
     }
 }
