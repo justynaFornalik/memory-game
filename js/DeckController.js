0c903c804c65af92ce221077f61346fbfbe4dcb4
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-07-11 14:13:34 +0200

    Create DeckController class

---
+++ js/DeckController.js
@@ -0,0 +1,34 @@
+class DeckController {
+
+    constructor() {
+        this.deck = [];
+    }
+
+    add(card) {
+        this.deck.push(card);
+    }
+
+    checkIfPair() {
+        let uncoveredCards = [];
+
+        for (let i = 0; i < this.deck.length; i++) {
+            if (this.deck[i].isCovered === false) {
+                uncoveredCards.push(this.deck[i])
+            }
+        }
+
+        if (uncoveredCards.length === 2) {
+            uncoveredCards[0].isCovered = true;
+            uncoveredCards[1].isCovered = true;
+
+        }
+
+        console.log(this.deck);
+
+
+    }
+
+
+}
+
+export let deck = new DeckController();
