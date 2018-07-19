fe98a075581b940229fa225d6a5ab42f733eb645
Justy <jfornalik@gmail.com>
2018-07-19 12:34:40 +0200

    Make for loop depend on photos array length

--- js/Main.js
+++ js/Main.js
@@ -1,11 +1,14 @@
 import Card from "./card/Card.js";
 import CardView from "./card/CardView.js";
 import CardController from "./card/CardController.js";
+import {photoDispenser} from "./helper/PhotosDispenser.js";
 
 
 let gameContainer = document.getElementById("game-container");
 
-for (let i=0; i<10; i++) {
+let initialCardAmmount = photoDispenser.photos.length;
+
+for (let i=0; i< initialCardAmmount; i++) {
     let card = new Card();
     let cardController = new CardController(card);
     let cardView = new CardView(cardController);
