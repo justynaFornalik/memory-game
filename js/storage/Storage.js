4a0041e94576e06db97601bc3f22fe368162dfa7
Justy <jfornalik@gmail.com>
2018-07-19 11:54:30 +0200

    Add Storage class

---
+++ js/storage/Storage.js
@@ -0,0 +1,33 @@
+import Score from "../score/Score.js";
+
+class Storage {
+    constructor(){
+        this.scores = new Set();
+        this.load();
+    }
+
+    add(score){
+        this.scores.add(score);
+        this.save();
+    }
+
+    remove(score){
+        this.scores.delete(score);
+        this.save();
+    }
+
+    save(){
+        localStorage.setItem('scores', JSON.stringify([...this.scores]))
+    }
+
+    load(){
+        let scores = JSON.parse(localStorage.getItem('scores'));
+        if(scores !== null){
+            for(let score of scores){
+                this.scores.add(Score.createFromObject(score));
+            }
+        }
+    }
+}
+
+export let storage = new Storage();
