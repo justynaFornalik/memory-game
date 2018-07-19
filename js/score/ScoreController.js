037ffe4f6c0765890fa88fc0e0a9d34c07cb48b0
Justy <jfornalik@gmail.com>
2018-07-19 13:43:02 +0200

    Add setScoreAttribute method and export

--- js/score/ScoreController.js
+++ js/score/ScoreController.js
@@ -1,5 +1,15 @@
-class ScoreController {
+export default class ScoreController {
     constructor(score) {
         this.score = score;
+    }
+
+    setScoreAttributes(calculatedPoints) {
+        let name = "Jan Nowak";
+        let rank = "1";
+        let points = calculatedPoints;
+
+        this.score.player = name;
+        this.score.rank = rank;
+        this.score.points = points;
     }
 }
